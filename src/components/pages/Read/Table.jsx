319c17c73524efa0ab2b99b5d72ea2bd99970a66
Lukas Daniel <60492975+LukasDniel@users.noreply.github.com>
2022-09-27 10:03:53 -0300

    Update Table.jsx

--- src/components/pages/Read/Table.jsx
+++ src/components/pages/Read/Table.jsx
@@ -21,13 +21,8 @@ export default function Tabela() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
               <TableCell component="th" scope="row">
-                {row.name}
+                Titulo
               </TableCell>
               <TableCell align="right">Teste</TableCell>
               <TableCell align="right">Teste</TableCell>
